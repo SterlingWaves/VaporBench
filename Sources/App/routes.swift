afdf20272d4e89eaa69729f250e9fcd7664a5e4e
Sterling Li <zli@pinterest.com>
2019-07-24 17:37:04 -0700

    added basic view rendering

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,4 +1,5 @@
 import Vapor
+import Leaf
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
@@ -8,8 +9,20 @@ public func routes(_ router: Router) throws {
     }
     
     // Basic "Hello, world!" example
-    router.get("hello") { req in
+    router.get("hello1") { req in
         return "Hello, world!"
+    }
+    
+    let values = [
+        "name": "Taylor Swift",
+        "city": "Nashville",
+        "vaporSkill": "11/10",
+        ]
+    router.get("hello") { req -> Future<View> in
+        return try req.view().render("home", values)
+    }
+    router.get("h") { req -> Future<View> in
+        return try req.view().render("home", ["score": 80])
     }
 
     // Example of configuring a controller
